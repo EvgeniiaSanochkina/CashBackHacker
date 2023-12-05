9f4d4e47112ca11eba49da5271ce3a0ee64aba2c
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2023-12-05 14:49:34 +0700

    junit4 is used, one test failed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,9 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -11,6 +12,15 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
         int expected = 100;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldCountRemainIf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
         assertEquals(actual, expected);
 
     }
